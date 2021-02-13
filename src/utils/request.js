5f64af293733dc62867b1b23c7266b58145264dc
artpumpkin <pumpkin.salah@gmail.com>
2021-02-13 21:36:44 +0100

    feat(utils): update requests

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-// import { sleep } from 'utils';
 
-export const GET = async (path, headers = {}) => {
-  // await sleep(4);
-  return await axios.get(`${process.env.REACT_APP_API_URL}${path}`, {
-    withCredentials: true,
-    headers,
-  });
+export const GET = async (path) => {
+  console.log(`${process.env.REACT_APP_API_URL}${path}`)
+  return await axios.get(`${process.env.REACT_APP_API_URL}${path}`);
 };
